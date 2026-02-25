636e6f9ef9563b4a69e238c19416c757e70701c8
DrFonzarelli <98223792+DrFonzarelli@users.noreply.github.com>
2026-02-25 18:45:49 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
-});
-
-// Qui puoi aggiungere logica per push notifiche se setuppi un server, ma per base salta
